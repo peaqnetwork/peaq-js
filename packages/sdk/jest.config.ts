ff33d21a9db4b2aae59e80076d2639234ebb4f67
lavish0000 <lavick100@gmail.com>
2023-04-25 17:11:25 +0530

    update: jest config to check js files

--- packages/sdk/jest.config.ts
+++ packages/sdk/jest.config.ts
@@ -1,11 +1,18 @@
 /* eslint-disable */
-export default {
+import type { Config } from 'jest';
+const config: Config = {
   displayName: 'sdk',
   preset: '../../jest.preset.js',
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json', useEsm: true }],
   },
+  "moduleNameMapper": {
+    "^(\\.\\.?\\/.+)\\.js$": "$1",
+  },
+  extensionsToTreatAsEsm: ['.ts'],
   moduleFileExtensions: ['ts', 'js', 'html', 'd.ts'],
   coverageDirectory: '../../coverage/packages/sdk',
-  collectCoverage: true
+  collectCoverage: true,
 };
+
+export default config;
