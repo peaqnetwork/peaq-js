bfc087aff74a236e84bda89dd7642dbe77a1fbe3
lavish0000 <lavick100@gmail.com>
2023-07-27 13:50:08 +0530

    update: add customDocumentFields in CreateDidOptions

--- packages/sdk/src/utils/index.ts
+++ packages/sdk/src/utils/index.ts
@@ -39,3 +39,10 @@ export const unsubscribeRuntimeVersion = async (api: ApiPromise) => {
 };
 
 export const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+export const generateRandomString = (length = 6) => {
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let result = '';
+  for (let i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+}
